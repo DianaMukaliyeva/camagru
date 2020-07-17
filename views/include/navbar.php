abd6784259fd02d4165c75278763e65571be7b45
= <diana.mukaliyeva@gmail.com>
2020-07-17 14:57:27 +0300

    chore:

---
+++ views/include/navbar.php
@@ -0,0 +1,35 @@
+<nav class="navbar is-fixed-topr is-primary">
+    <div class="container is-primary">
+        <div class="navbar-brand">
+            <a class="navbar-item" href="index.php">
+                <img src="assets/img/images/logo4.png" alt="logo" width="112" height="50">
+            </a>
+
+            <a role="button" class="navbar-burger burger" data-target="navMenu">
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+                <span aria-hidden="true"></span>
+            </a>
+        </div>
+
+        <div id="navMenu" class="navbar-menu">
+            <div class="navbar-start">
+                <a class="navbar-item is-active">Home</a>
+                <a class="navbar-item">Gallery</a>
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <div class="buttons">
+                        <a class="button is-primary">
+                            <strong>Sign up</strong>
+                        </a>
+                        <a class="button is-light">
+                            Log in
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</nav>
