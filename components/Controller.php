1dd45d91bd23f8662e06d92b2c89715b4c92411a
= <diana.mukaliyeva@gmail.com>
2020-07-22 20:18:19 +0300

    Add Controller

---
+++ components/Controller.php
@@ -0,0 +1,32 @@
+<?php
+class Controller {
+    public function redirect($url) {
+        header("Location: " . URLROOT . "/$url");
+        header('Connection: close');
+        exit;
+    }
+
+    public function getModel($model) {
+        if (file_exists('models/' . $model . '.php')) {
+            // Require model file
+            require_once 'models/' . $model . '.php';
+        } else {
+            // Model does not exist
+            echo "something went wrong";
+            return null;
+        }
+        // Instatiate model
+        return new $model();
+    }
+
+    // Load view
+    public function renderView($view, $data = []) {
+        // Check for view file
+        if (file_exists('views/' . $view . '.php')) {
+            require_once 'views/' . $view . '.php';
+        } else {
+            // View does not exist
+            echo "something went wrong";
+        }
+    }
+}
