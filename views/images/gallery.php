77e276493c34905b9ef105cb12eedb230001ef8d
= <diana.mukaliyeva@gmail.com>
2020-07-23 22:51:42 +0300

    Modify image views

--- views/images/gallery.php
+++ views/images/gallery.php
@@ -1,6 +1,25 @@
 <?php require 'views/include/header.php' ?>
-
-<div class="container">
-    <p>This is a gallery</p>
+<div class="mt-3 input-group justify-content-center">
+    <div id="radioBtn" class="btn-group">
+        <a class="sort_images btn btn-light active" data-toggle="sort_image" data-title="newest">Newest</a>
+        <a class="sort_images btn btn-light" data-toggle="sort_image" data-title="popular">Popular</a>
+    </div>
+    <input type="hidden" name="sort_image" id="sort_image">
 </div>
+<?php if (!empty($images)) : ?>
+    <?php foreach ($data['posts'] as $post) : ?>
+        <div class="card card-body mb-3">
+            <h4 class="card-title"><?php echo $post->title; ?></h4>
+            <div class="bg-light p-2 mb-3">
+                Written by <?php echo $post->name; ?> on <?php echo $post->postCreated; ?>
+            </div>
+            <p class="card-text"><?php echo $post->body; ?></p>
+            <a href="<?php echo URLROOT; ?>/posts/show/<?php echo $post->postId; ?>" class="btn btn-dark">More</a>
+        </div>
+    <?php endforeach; ?>
+<?php else : ?>
+    <div class="col-md-6 m-auto text-center pt-5">
+        <h3>No photo yet...</h3>
+    </div>
+<?php endif; ?>
 <?php require 'views/include/footer.php' ?>
