8f06640376f8b367d2a56f010401a1f35f538f32
= <diana.mukaliyeva@gmail.com>
2020-08-12 20:04:28 +0300

    feat: :sparkles: Add gallery ordered by popularity

--- models/Image.php
+++ models/Image.php
@@ -1,9 +1,20 @@
 <?php
 class Image {
 
-    // Get all images
+    // Get all images ordered by date
     public function getImagesByDate() {
         $result = Db::queryAll('SELECT * FROM `images` ORDER BY `created_at`');
+        return $result;
+    }
+
+    // Get all images ordered by likes
+    public function getImagesByLikes() {
+        $result = Db::queryAll(
+            'SELECT images.id, images.image_path, images.created_at, images.user_id,
+            COUNT(likes.id) AS `likes_amount`
+            FROM images LEFT JOIN likes ON likes.image_id = images.id
+            GROUP BY images.id ORDER BY `likes_amount` DESC'
+        );
         return $result;
     }
 
